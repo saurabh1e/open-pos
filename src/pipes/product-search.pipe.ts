7f55da4cc537509cfb38595aa1730741d8d409d3
saurabh <saurabh.1e1@gmail.com>
2017-02-09 20:14:54 +0530

    pipe to iterate over objects

--- src/pipes/product-search.pipe.ts
+++ src/pipes/product-search.pipe.ts
@@ -112,3 +112,14 @@ export class ProductDistributorPipe implements PipeTransform {
   }
 
 }
+
+@Pipe({name: 'keys'})
+export class KeysPipe implements PipeTransform {
+  transform(value:{}, args:string[]) : any {
+    let keys = [];
+    for (let key in value) {
+      keys.push({key: key, value: value[key]});
+    }
+    return keys;
+  }
+}
