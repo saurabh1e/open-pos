546f8c47159c675e690859fe295f18b2f6b188f5
saurabh <saurabh.1e1@gmail.com>
2017-02-22 22:59:05 +0530

    order table

--- src/app/reporting/order/order.component.ts
+++ src/app/reporting/order/order.component.ts
@@ -3,6 +3,7 @@ import {TdDialogService} from "@covalent/core";
 import {RESTService} from "@covalent/http";
 import {TdDataTableColumn} from "../../td-data-table-column";
 import {OrdersService} from "../../../services/orders.service";
+import {DateFormatter} from "@angular/common/src/pipes/intl";
 
 
 @Component({
@@ -14,8 +15,14 @@ export class OrderComponent implements OnInit {
 
   columns: TdDataTableColumn[] = [
     {name: 'id', label: 'id', sortable: true},
-    {name: 'name', label: 'Name', sortable: true},
-    {name: 'retail_shop.name', 'label': 'Shop', nested: true}
+    {name: 'customer.name', label: 'Customer', sortable: true, nested: true },
+    {name: 'customer.mobile_number', label: 'Number', sortable: true, nested: true },
+    {name: 'retail_shop.name', 'label': 'Shop', nested: true },
+    {name: 'total', label: 'Total', numeric: true, format:v=>v.toFixed(2)},
+    {name: 'amount_due', label: 'Due', numeric: true, format:v=>v.toFixed(2)},
+    {name: 'items_count', label: 'Items', numeric: true},
+    {name: 'auto_discount', label: 'Discount', numeric: true},
+    {name: 'created_on', label: 'Date', format:v=>DateFormatter.format(new Date(v), 'en', 'dd/MM/yy hh:mm')},
   ];
 
   constructor(
