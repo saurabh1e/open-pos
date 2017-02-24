34afe3adb1f7387e813ffccfbb53eec7de77a76a
saurabh <saurabh.1e1@gmail.com>
2017-02-24 12:55:01 +0530

    updated deployment script

--- scripts/ghpages-deploy.sh
+++ scripts/ghpages-deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+
+git branch -f gh-pages
+git checkout gh-pages
+
 echo 'Replace api endpoint with https://open-point-of-sale.herokuapp.com/api/v1/'
 
 cat <<'EOF' >> api.config.ts
@@ -8,11 +13,10 @@ EOF
 mv src/config/api.config.ts api.config.ts.default
 mv api.config.ts src/config/api.config.ts
 
-git branch -f gh-pages
-git checkout gh-pages
+ng build --base-href /open-pos-frontend/
+
 echo 'Created and Checked out gh-pages branch'
 
-git reset --hard origin/master
 
 cp -r dist/* .
 echo 'Copied dist/ directory into root dir'
@@ -23,3 +27,4 @@ echo 'Commiting files'
 git commit -a -m 'gh-pages update'
 echo 'Pushing files into gh-pages branch'
 git push origin gh-pages --force
+git checkout origin/master
