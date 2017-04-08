83660e3ce47092ccc5b44f374b418f5b604ab583
saurabh <saurabh.1e1@gmail.com>
2017-04-08 22:37:23 +0530

    added main route

---
+++ src/app/main-route/main-route.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-route',
+  templateUrl: './main-route.component.html',
+  styleUrls: ['./main-route.component.scss']
+})
+export class MainRouteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
